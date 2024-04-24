b33b54100e163d5f9d4c22fa62a9cb65980cb8b4
Om Jadav <omjadav04@gmail.com>
2024-04-24 23:40:41 +0530

    final commit
    ready to deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,70 @@
-import logo from './logo.svg';
-import { Homepage } from './pages/Homepage';
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/sections/Hero";
+import Skills from "./components/sections/Skills";
+import { AnimatePresence } from "framer-motion";
+import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
+import Footer from "./components/sections/Footer";
+import ProjectDetails from "./components/Dialog/ProjectDetails";
+import { useState } from "react";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(254, 76, 6, 0.15) 0%,
+      rgba(254, 76, 6, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(254, 76, 6, 0) 50%,
+      rgba(254, 76, 6, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 
 function App() {
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
   return (
-    <>
-      <Homepage />
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <AnimatePresence>
+            <div>
+              <Hero />
+              <Wrapper>
+                <Skills />
+              </Wrapper>
+              <Projects openModal={openModal} setOpenModal={setOpenModal} />
+              <Wrapper>
+                <Contact />
+              </Wrapper>
+              <Footer />
+
+              {openModal.state && (
+                <ProjectDetails
+                  openModal={openModal}
+                  setOpenModal={setOpenModal}
+                />
+              )}
+            </div>
+          </AnimatePresence>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
